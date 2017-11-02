e68d4b2b8b54b7d602524f14ef062de8fb1e3285
James Porter <porterjames1993@yahoo.com>
2017-11-02 11:32:07 -0700

    updated sanity/ it works

--- source/__tests__/sanity.js
+++ source/__tests__/sanity.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 
 import ReactHome from '../ReactHome';
 import HomeButtons from "../HomeButtons";
+import ElfDebugEnzyme from '../ElfDebugEnzyme';
+const elfDebugEnzyme = new ElfDebugEnzyme(true, 'Foo.test.js');
+
+import '../temp-poly-fills';
+//import raf from '../temp-poly-fills';
+
 
 import {configure, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -26,7 +32,7 @@ describe('basic sanity test', function() {
 
     it('renders default value of H1 tag', () => {
         const wrapper = shallow(<ReactHome/>);
-        const nineSign = <h1> An H1 element in a React Component</h1>
+        const nineSign = <h1>An H1 element in a React Component</h1>
         elfDebugEnzyme.getLast(wrapper, 'h1', true);
         expect(wrapper.contains(nineSign)).toEqual(true);
     })
